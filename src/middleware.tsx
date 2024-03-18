03bb204deda291f77593b14a7f1ca9f3a6df18b7
Colin Gillingham <colingillingham@gmail.com>
2024-03-18 10:46:36 -0400

    fix

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,5 +1,11 @@
 import { NextResponse, NextRequest } from "next/server";
-import { getToken } from "next-auth/jwt";
+import { getToken, JWT } from "next-auth/jwt";
+
+declare module "next/server" {
+  interface NextRequest {
+    nextauth?: { token: JWT }; // Use JWT type
+  }
+}
 
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
