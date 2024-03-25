7953ff5552a5a98fe14aeb920ee1ccba79b70823
Colin Gillingham <colingillingham@gmail.com>
2024-03-24 20:40:46 -0400

    analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,11 +1,13 @@
 // pages/_app.tsx
 import { SessionProvider } from "next-auth/react";
+import { Analytics } from "@vercel/analytics/react"
 import "../styles/globals.css"; // or wherever your global styles are
 
 function MyApp({ Component, pageProps }) {
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
+      <Analytics />
     </SessionProvider>
   );
 }
