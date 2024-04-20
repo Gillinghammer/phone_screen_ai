323172b0e79b0ca4e3b906c7ca10324242166e64
Colin Gillingham <colingillingham@gmail.com>
2024-04-20 11:18:07 -0400

    foo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -205,7 +205,7 @@ const Home: NextPage = () => {
           </div>
         </div>
       </section>
-      <ProductScreenshots />
+      {/* <ProductScreenshots /> */}
       <section className="py-16 bg-yellow-200 overflow-hidden  border-t-4 border-gray-300">
         <div className="relative max-w-xl mx-auto text-center px-4 sm:px-6 lg:px-8">
           <h2 className="text-3xl font-extrabold tracking-tight text-gray-800 sm:text-4xl">
