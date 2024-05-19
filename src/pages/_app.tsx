a1c0698a3abf84c46baae49387f130e0fc74761f
Colin Gillingham <colingillingham@gmail.com>
2024-05-19 14:45:19 -0400

    fixed type issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,13 +11,18 @@ import { PostHogProvider } from "posthog-js/react";
 
 if (typeof window !== "undefined") {
   // checks that we are client-side
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host:
-      process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
-    loaded: (posthog) => {
-      if (process.env.NODE_ENV === "development") posthog.debug(); // debug mode in development
-    },
-  });
+  const apiKey = process.env.NEXT_PUBLIC_POSTHOG_KEY;
+  if (apiKey) {
+    posthog.init(apiKey, {
+      api_host:
+        process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+      loaded: (posthog) => {
+        if (process.env.NODE_ENV === "development") posthog.debug(); // debug mode in development
+      },
+    });
+  } else {
+    console.warn("PostHog API key is missing.");
+  }
 }
 
 const fontSans = FontSans({
