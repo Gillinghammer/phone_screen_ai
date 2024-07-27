26768712e91f280dc8dbbbd3b579c136d69854a2
Colin Gillingham <colingillingham@gmail.com>
2024-07-27 10:08:09 -0400

    rm redirect rules

--- next.config.mjs
+++ next.config.mjs
@@ -1,33 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "phonescreen.ai",
-          },
-        ],
-        destination: "https://app.phonescreen.ai/:path*",
-        permanent: true,
-      },
-      // Exclude the root path from redirection
-      {
-        source: "/",
-        has: [
-          {
-            type: "host",
-            value: "phonescreen.ai",
-          },
-        ],
-        destination: "https://phonescreen.ai",
-        permanent: false,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
